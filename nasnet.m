733ab0f59502042160017fc3f0ac1e3c2a263f0d
DEFINE000 <83963366+DEFINE000@users.noreply.github.com>
2023-03-27 13:32:29 +0800

    Update nasnet.m

--- nasnet.m
+++ nasnet.m
@@ -9,15 +9,15 @@ system("git init");
 system("git lfs install");
 system('git config --global user.email "2978985731@qq.com"')
 system('git config --global user.name "vc"')
+
 system("git lfs track *")
 system("git add .gitattributes")
 system('git commit -m "first"');
-system('git remote add origin https://github.com/DEFINE000/NET.git')
+system('git remote add origin git@github.com:DEFINE000/NET.git')
 system("git push origin master");
 system("git add *");
 system('git commit -m "nb"');
-system("git push origin master")
-
+system("git push -u origin master")
 
 
 
